03f0294408b076c2b3c028187311b9309b714d84
ryzXD <111454488+ryz-code@users.noreply.github.com>
2023-06-13 04:48:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@
 from os import getenv
 
 
-API_KEY = "EDVHJP"
+API_KEY = "PEOHFI"
 
-if not API_KEY or API_KEY == "EDVHJP" or API_KEY == "EDVHJP":
+if not API_KEY or API_KEY == "PEOHFI" or API_KEY == "PEOHFI":
     API_KEY = getenv("API_KEY")
 
     if not API_KEY:
