573084a8ea3ef4a80d668c08cd7a2e29ba2e4591
ryzXD <111454488+ryz-code@users.noreply.github.com>
2023-06-13 04:47:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@
 from os import getenv
 
 
-API_KEY = "Paste Your Key Here"
+API_KEY = "EDVHJP"
 
-if not API_KEY or API_KEY == "Paste Your Key Here" or API_KEY == "":
+if not API_KEY or API_KEY == "EDVHJP" or API_KEY == "EDVHJP":
     API_KEY = getenv("API_KEY")
 
     if not API_KEY:
